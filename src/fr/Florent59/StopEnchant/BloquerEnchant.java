8688ca9cfcf026f37fb4ee9bd9f51372cf2022b3
Florent <florent.lesage.prioux@gmail.com>
2018-02-16 21:43:42 +0100

    Affichage d'un message dans la console.

--- src/fr/Florent59/StopEnchant/BloquerEnchant.java
+++ src/fr/Florent59/StopEnchant/BloquerEnchant.java
@@ -1,6 +1,7 @@
 package fr.Florent59.StopEnchant;
 
 import org.bukkit.GameMode;
+import org.bukkit.craftbukkit.libs.jline.internal.Log;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,6 +16,7 @@ public class BloquerEnchant implements Listener {
 		if(!p.isOp() && !p.getGameMode().equals(GameMode.CREATIVE)){
 		e.setCancelled(true);
 		p.sendMessage("Vous n'avez pas l'autorisation d'enchanter !");
+		Log.info(p.getName()+ " a essayé d'enchanter !");
 		}
 		
 	}
